af3d1789c47c5225fa2ad58ccf34c39abf6b5c06
Enaium <wyy-666@hotmail.com>
2021-07-25 17:15:38 +0800

    Edit

--- src/main/java/cn/enaium/treecutter/Events.java
+++ src/main/java/cn/enaium/treecutter/Events.java
@@ -10,21 +10,41 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * @author Enaium
  */
-public class EventHandles implements Listener {
+public class Events implements Listener {
 
     private final List<Material> logs = Arrays.asList(Material.WARPED_STEM, Material.CRIMSON_STEM, Material.OAK_LOG, Material.BIRCH_LOG, Material.SPRUCE_LOG, Material.JUNGLE_LOG, Material.DARK_OAK_LOG, Material.ACACIA_LOG);
     private final List<Material> leaves = Arrays.asList(Material.NETHER_WART_BLOCK, Material.WARPED_WART_BLOCK, Material.OAK_LEAVES, Material.BIRCH_LEAVES, Material.SPRUCE_LEAVES, Material.JUNGLE_LEAVES, Material.DARK_OAK_LEAVES, Material.ACACIA_LEAVES);
+    private final List<Material> target = new ArrayList<>();
     private final List<Material> tools = Arrays.asList(Material.NETHERITE_AXE, Material.DIAMOND_AXE, Material.GOLDEN_AXE, Material.IRON_AXE, Material.STONE_AXE, Material.WOODEN_AXE);
+
+    private final JavaPlugin javaPlugin;
+
+    public Events(JavaPlugin javaPlugin) {
+        this.javaPlugin = javaPlugin;
+    }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
+        target.clear();
+        if (javaPlugin.getConfig().getBoolean("log")) {
+            target.addAll(logs);
+        }
+
+        if (javaPlugin.getConfig().getBoolean("leave")) {
+            target.addAll(leaves);
+        }
+
+
         final Block block = event.getBlock();
         final Player player = event.getPlayer();
         if (!logs.contains(block.getBlockData().getMaterial())) return;
@@ -44,7 +64,7 @@ public class EventHandles implements Listener {
             for (int y = -1; y < 2; y++) {
                 for (int z = -1; z < 2; z++) {
                     Block relative = block.getRelative(x, y, z);
-                    if (logs.contains(relative.getBlockData().getMaterial()) || leaves.contains(relative.getBlockData().getMaterial())) {
+                    if (target.contains(relative.getBlockData().getMaterial())) {
 
                         ItemMeta meta = hand.getItemMeta();
                         if (meta != null) {
--- src/main/java/cn/enaium/treecutter/TreeCutter.java
+++ src/main/java/cn/enaium/treecutter/TreeCutter.java
@@ -2,6 +2,7 @@ package cn.enaium.treecutter;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
 import java.util.logging.Level;
 
 public final class TreeCutter extends JavaPlugin {
@@ -9,11 +10,12 @@ public final class TreeCutter extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().log(Level.INFO, "TreeCutter is enable");
-        getServer().getPluginManager().registerEvents(new EventHandles(), this);
+        getServer().getPluginManager().registerEvents(new Events(this), this);
     }
 
     @Override
     public void onDisable() {
+        saveConfig();
         getLogger().log(Level.INFO, "TreeCutter is disable");
     }
 }
